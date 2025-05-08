4343d15dada9435cfca526f6dbae1d6d72f519c0
Johnathan Fludd <Johnathanfludd@gmail.com>
2025-05-08 14:39:13 -0500

    building out buckets

--- terraform-nextjs/state.tf
+++ terraform-nextjs/state.tf
@@ -1,9 +1,11 @@
 
 terraform {
   backend "s3" {
-    bucket = "fludd-terraform-state"
+    bucket = "my-web-terraform-state"
     key    = "global/s3/terraform.tfstate"
-    dynamodb_table = "terraform-web-site"
     region = "us-east-1"
+    dynamodb_table = "terraform-lock-file"
   }
 }
+
+
